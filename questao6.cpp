3a3dc908f7b7becfb64fedb59438a1f23fbdf1f5
Leonardo Padilha Fontana <leonardopad2@gmail.com>
2021-05-28 12:52:24 -0300

    Initial commit

---
+++ questao6.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int num1 = 0;
+	cout << "digite um numero: ";
+	cin >> num1;
+	
+    if (num1 >= 0){
+    cout << sqrt(num1) << " e a raiz de " << num1;
+	}
+	if (num1 < 0){
+		int quadrado = 0;
+		quadrado = num1 * num1;
+		cout << "O quadrado de " << num1 << " e " << quadrado;
+		
+	}
+    return 0;
+    //comando em tecla
+	system ("pause");
+}
---
+++ questao6_2.0.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int num1 = 0;
+	
+	cout << "digite um numero: \n";
+	cin >> num1;
+	
+	if (num1 % 2 != 0){
+		num1 = num1 * num1 /2;
+		cout >> "A raiz é " + num1;
+		
+		
+	}
+	
+	
+		   
+		
+		}	
+
